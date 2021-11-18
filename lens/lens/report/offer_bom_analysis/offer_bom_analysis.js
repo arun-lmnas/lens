3405d32ac4f07260ef698ef61d2e16db44f97413
root <root@beeinc-nectar-ubunto.us-central1-a.c.ultra-envoy-277306.internal>
2021-11-18 20:20:37 +0530

    Track Reprots'

---
+++ lens/lens/report/offer_bom_analysis/offer_bom_analysis.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, LMNAs Cloud Solutions and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Offer BOM Analysis"] = {
+	"filters": [
+
+	]
+};
