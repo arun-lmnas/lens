67a1b5aa1ac8e2200f66f1cd9636922d2f9f3292
Arunkumar Ganesan <67154864+arun-lmnas@users.noreply.github.com>
2021-11-19 09:48:50 +0530

    Update offer_calculation_beta.py

--- lens/lens/report/offer_calculation_beta/offer_calculation_beta.py
+++ lens/lens/report/offer_calculation_beta/offer_calculation_beta.py
@@ -3,26 +3,69 @@
 
 # import frappe
 
+def query():
+  return [
+    { "xAxisField":  30, "yAxisField": 4156.78 },
+    { "xAxisField":  45, "yAxisField": 3908.34 },
+    { "xAxisField":  60, "yAxisField": 5632.87 },
+    { "xAxisField":  75, "yAxisField": 5698.21 },
+    { "xAxisField":  90, "yAxisField": 2321.54 },
+    { "xAxisField": 105, "yAxisField": 4446.88 }
+  ]
+
 def get_columns():
   return [
     {
-      "fieldname": "someField",
+      "fieldname": "xAxisField",
       "fieldtype": "Int",
-      "label": "Some Field",
+      "label": "X-Axis",
+      "width": 100
+    },
+    {
+      "fieldname": "yAxisField",
+      "fieldtype": "Currency",
+      "label": "Y-Axis",
       "width": 100
     },
   ]
 
-def get_data(fltr):
-  values = [
-    { "someField":  30 },
-    { "someField":  45 },
-    { "someField":  60 },
-    { "someField":  75 },
-    { "someField":  90 },
-    { "someField": 105 }
+def get_data(data, fltr):
+  return [ value for value in data if value["xAxisField"] > int(fltr.xAxisField) ]
+
+def get_chart(data, columns, fltr):
+  attributes = [d.get("fieldname") for d in columns]
+
+  dimensions = [
+    [
+      value.get(attr) for value in data if value["xAxisField"] > int(fltr.xAxisField)
+    ] for attr in attributes
   ]
-  return [ v for v in values if v["someField"] > int(fltr.someField) ]
+
+  L = 0
+  V = L + 1
+
+  chart = {
+    'data': {
+      'labels': dimensions[L],
+      'datasets': [
+        {
+          'name': 'Y Value',
+        'values': dimensions[V]
+        }
+      ]
+    },
+    'isNavigable': 1,
+    'type': 'bar'
+  }
+
+  return chart
 
 def execute(filters=None):
-  return get_columns(), get_data(filters) 
+  query_result = query()
+
+  columns = get_columns()
+  data = get_data(query_result, filters)
+  message = "Here is a message"
+  chart = get_chart(query_result, columns, filters)
+
+  return columns, data, message, chart
